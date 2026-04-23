3b6a447a0729a33c2ec08f018a8b6301c7c2d2d4
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-04-23 10:43:51 -0300

    feat: add app

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from "react";
+import { View, Text, Image, ScrollView, TextInput } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import api from './src/services/api';
+import { globalStyles } from './src/styles/globalStyles';
 
 export default function App() {
+  const [filmes, setFilmes] = useState([]);
+  const [pesquisa, setPesquisa] = useState('');
+
+  useEffect(() => {
+    async function carregarFilmes() {
+      if (pesquisa.trim() !== '') {
+        try {
+          const response = await api.get(encodeURIComponent(pesquisa));
+          setFilmes(response.data);
+        } catch (error) {
+          console.log('erro na requisição: ', error);
+        }
+      } else {
+        setFilmes([]);
+      }
+    }
+    carregarFilmes();
+  }, [pesquisa]);
+
+  const handlePesquisa = (texto) => {
+    setPesquisa(texto);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={globalStyles.container}>
+      <TextInput
+        style={globalStyles.input}
+        placeholder="digite o nome do filme"
+        value={pesquisa}
+        onChangeText={handlePesquisa}
+      />
+      <Text style={globalStyles.titulo}>
+        Resultado(s) da pesquisa
+      </Text>
+
+      <ScrollView>
+        {filmes.map((filme) => (
+          <View key={filme.show.id} style={globalStyles.card}>
+            {filme.show.image && (
+              <Image
+                source={{ uri: filme.show.image.medium }}
+                style={globalStyles.imagem}
+                resizeMode='cover'
+              />
+            )}
+            <View style={globalStyles.infoContainer}>
+              <Text style={globalStyles.tituloFilme}>
+                {filme.show.name}
+              </Text>
+              <Text style={globalStyles.url}>
+                {filme.show.url}
+              </Text>
+            </View>
+          </View>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
